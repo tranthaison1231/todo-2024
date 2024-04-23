1370f0fe47571a042a1abb207850bdd92ecf0b79
son.tran <son.tran@team.enouvo.com>
2024-04-23 22:20:44 +0700

    feat(master): update todolist

--- main.js
+++ main.js
@@ -5,22 +5,51 @@ const count = document.getElementById("count");
 const clearCompleteBtn = document.getElementById("clear-complete");
 const footer = document.getElementById("footer");
 
-function renderListItem() {
-  const totalItemLeft = Array.from(list.children).filter((item) => {
-    return !item.classList.contains("line-through");
-  }).length;
+let TODOS = [];
 
-  count.textContent = `${totalItemLeft} items left!`;
+function rennderTodo(todo) {
+  const li = document.createElement("li");
+  li.dataset.id = todo.id;
+  li.className = "group p-4 flex items-center border border-t-1";
+  li.insertAdjacentHTML(
+    "afterbegin",
+    `
+        ${
+          todo.completed
+            ? '<svg data-todo="toggle" class="text-green-500 cursor-pointer w-10 h-10 mr-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-circle"><circle cx="12" cy="12" r="10"/><path d="m9 12 2 2 4-4"/></svg>'
+            : '<svg data-todo="toggle" class="cursor-pointer w-10 h-10 mr-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-circle"><circle cx="12" cy="12" r="10"/></svg>'
+        }
+        <div class="flex justify-between flex-1 ${todo.completed ? "line-through" : ""}">
+          <span data-todo="value" contenteditable="true">${todo.content}</span>
+          <svg data-todo="remove" class="hidden group-hover:block cursor-pointer hover:text-red-500" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-x"><path d="M18 6 6 18"/><path d="m6 6 12 12"/>
+          </svg>
+        </div>
 
-  if (list.children.length === 0) {
+      `,
+  );
+  return li;
+}
+
+function renderTodos() {
+  const hash = window.location.hash;
+  let filterTodos = [...TODOS];
+
+  if (TODOS.length === 0) {
     footer.classList.add("hidden");
     list.classList.add("hidden");
   } else {
     footer.classList.remove("hidden");
     list.classList.remove("hidden");
   }
+  if (hash === "#/active")
+    filterTodos = TODOS.filter((todo) => !todo.completed);
+  else if (hash === "#/complete")
+    filterTodos = TODOS.filter((todo) => todo.completed);
 
-  const hash = window.location.hash;
+  list.replaceChildren(...filterTodos.map((todo) => rennderTodo(todo)));
+
+  const totalItemLeft = TODOS.filter((todo) => !todo.completed).length;
+  count.textContent = `${totalItemLeft} items left!`;
 
   document.querySelectorAll('[data-todo="filters"] a').forEach((el) => {
     if (el.matches(`[href="${hash}"]`)) {
@@ -29,104 +58,60 @@ function renderListItem() {
       el.classList.remove("checked");
     }
   });
-
-  switch (hash) {
-    case "#/active":
-      Array.from(list.children).forEach((item) => {
-        if (item.classList.contains("line-through")) {
-          item.classList.add("hidden");
-        } else {
-          item.classList.remove("hidden");
-        }
-      });
-      break;
-    case "#/completed":
-      Array.from(list.children).forEach((item) => {
-        if (item.classList.contains("line-through")) {
-          item.classList.remove("hidden");
-        } else {
-          item.classList.add("hidden");
-        }
-      });
-      break;
-    default:
-      Array.from(list.children).forEach((item) => {
-        item.classList.remove("hidden");
-      });
-      break;
-  }
 }
 
 input.onkeyup = function (e) {
   const value = e.target.value;
   if (e.key === "Enter" && input.value.trim(" ") !== "") {
-    const li = `
-      <li class="group p-4 flex items-center border border-t-1">
-        <svg data-todo="toggle" class="cursor-pointer w-10 h-10 mr-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-circle"><circle cx="12" cy="12" r="10"/></svg>
-        <div class="flex justify-between flex-1">
-          <span contenteditable="true">${value}</span>
-          <svg data-todo="remove" class="hidden group-hover:block cursor-pointer hover:text-red-500" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-x"><path d="M18 6 6 18"/><path d="m6 6 12 12"/>
-          </svg>
-        </div>
-      </li>`;
-    list.insertAdjacentHTML("afterbegin", li);
+    TODOS.push({
+      id: crypto.randomUUID(),
+      content: value,
+      completed: false,
+    });
     input.value = "";
-    renderListItem();
+    renderTodos();
   }
 };
 
 list.onclick = function (e) {
   if (e.target.matches("[data-todo='remove']")) {
-    e.target.closest("li").remove();
+    TODOS = TODOS.filter(
+      (todo) => todo.id !== e.target.closest("li").dataset.id,
+    );
   }
 
   if (e.target.matches("[data-todo='toggle']")) {
-    if (e.target.classList.contains("text-green-500")) {
-      e.target.classList.remove("text-green-500");
-      e.target.children[0].remove();
-      e.target.closest("li").classList.remove("line-through");
-    } else {
-      e.target.closest("li").classList.add("line-through");
-      e.target.classList.add("text-green-500");
-      e.target.insertAdjacentHTML("afterbegin", `<path d="m9 12 2 2 4-4"/>`);
-    }
+    const id = e.target.closest("li").dataset.id;
+    TODOS = TODOS.map((item) => {
+      if (item.id === id) {
+        return {
+          ...item,
+          completed: !item.completed,
+        };
+      }
+      return item;
+    });
   }
-  renderListItem();
+  renderTodos();
 };
 
 selectAllBtn.onclick = function () {
-  const totalItemLeft = Array.from(list.children).filter((item) => {
-    return !item.classList.contains("line-through");
-  }).length;
-
-  if (totalItemLeft === 0) {
-    Array.from(list.children).forEach((item) => {
-      item.classList.remove("line-through");
-      item.children[0].classList.remove("text-green-500");
-      item.children[0].children[0].remove();
-    });
-  } else {
-    Array.from(list.children).forEach((item) => {
-      if (!item.classList.contains("line-through")) {
-        item.classList.add("line-through");
-        item.children[0].classList.add("text-green-500");
-        item.children[0].insertAdjacentHTML(
-          "afterbegin",
-          `<path d="m9 12 2 2 4-4"/>`,
-        );
-      }
-    });
-  }
-  renderListItem();
+  TODOS = TODOS.map((item) => {
+    return {
+      ...item,
+      completed: true,
+    };
+  });
+  renderTodos();
 };
 
 clearCompleteBtn.onclick = function () {
-  list.querySelectorAll(".line-through").forEach((item) => {
-    item.remove();
-  });
-  renderListItem();
+  TODOS = TODOS.filter((todo) => !todo.completed);
+  renderTodos();
 };
 
 window.addEventListener("hashchange", () => {
-  renderListItem();
+  renderTodos();
 });
+
+renderTodos();
